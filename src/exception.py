b53eec8608f87c74b6913881e2689b17f6888ae7
WinnieKiarago <kiaragowinnie@ymail.com>
2024-05-28 14:39:55 +0300

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys): #creates a personalized error message
     _,_,exc_tb = error_detail.exc_info()#this is the execution info. details of error
